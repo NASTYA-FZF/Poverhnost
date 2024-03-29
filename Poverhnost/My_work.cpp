79d35f4ccd74329acb3bdba8471a09d1347581c9
NASTYA_FZF <2002marev@gmail.com>
2024-03-29 16:49:22 +0300

    end

--- Poverhnost/My_work.cpp
+++ Poverhnost/My_work.cpp
@@ -19,15 +19,17 @@ struct my_polygon
 {
 	double l;
 	math3d::vec4 v1, v2, v3, v4;
-	Color color_polygon;
 
-	my_polygon(double x, double y, double stepX, double stepY, double aroundY, double aroundZ, double x0)
+	my_polygon(double x, double y, double stepX, double stepY)
 	{
 		v1 = math3d::vec4(x, y, FuncZ(x, y));
 		v2 = math3d::vec4(x + stepX, y, FuncZ(x + stepX, y));
 		v3 = math3d::vec4(x + stepX, y + stepY, FuncZ(x + stepX, y + stepY));
 		v4 = math3d::vec4(x, y + stepY, FuncZ(x, y + stepY));
+	}
 
+	void RotatePolygon(double aroundY, double aroundZ, double x0)
+	{
 		math3d::mat4 my_mat = math3d::mat4();
 		my_mat.rotateY(aroundY).rotateZ(aroundZ).perspective(10);
 
@@ -41,8 +43,6 @@ struct my_polygon
 		double centerZ = (v1.z() + v2.z() + v3.z() + v4.z()) / 4;
 
 		l = sqrt((centerX - x0) * (centerX - x0) + centerY * centerY + centerZ * centerZ);
-
-		color_polygon = Color::Coral;
 	}
 
 	PointF* GetPointF()
@@ -56,6 +56,12 @@ struct my_polygon
 		return points;
 	}
 };
+
+std::vector<my_polygon> RotateAllPolygons(std::vector<my_polygon> my_pl, double aroundY, double aroundZ, double maxX)
+{
+	for (auto& pl : my_pl) pl.RotatePolygon(aroundY, aroundZ, maxX);
+	return my_pl;
+}
 
 void SorterPolygons(std::vector<my_polygon>& polygons)
 {
@@ -84,10 +90,28 @@ App my_app;
 class Win : public CFrameWnd
 {
 	ULONG_PTR token;
-	double aroundZ;
-	double aroundY;
+	double aroundZ, aroundY, minX, maxX, minY, maxY;
+	std::vector<my_polygon> polygons, cur_polygons;
 public:
-	Win() { Create(NULL, L"Поверхность"); GdiplusStartupInput input;  Status s; s = GdiplusStartup(&token, &input, NULL); if (s != Ok) MessageBox(L"s != Ok", L"Error", NULL); }
+	Win() 
+	{ 
+		Create(NULL, L"Поверхность"); 
+		GdiplusStartupInput input;  
+		Status s; 
+		s = GdiplusStartup(&token, &input, NULL); 
+		if (s != Ok) MessageBox(L"s != Ok", L"Error", NULL);
+
+		maxX = 6.;
+		minX = -6.;
+		maxY = 4.;
+		minY = -4.;
+		int num_polygon = 30;
+		double stepX = (maxX - minX) / num_polygon, stepY = (maxY - minY) / num_polygon;
+		for (double x = -2.; x < 2.; x += stepX)
+			for (double y = -2.; y < 2.; y += stepY)
+				polygons.push_back(my_polygon(x, y, stepX, stepY));
+	}
+
 	~Win() { GdiplusShutdown(token); };
 	DECLARE_MESSAGE_MAP()
 		afx_msg void OnPaint();
@@ -113,11 +137,6 @@ void Win::OnPaint()
 	CPaintDC dc(this); // device context for painting
 	// TODO: добавьте свой код обработчика сообщений
 	// Не вызывать CFrameWnd::OnPaint() для сообщений рисования
-	
-	double maxX = 6., minX = -6., maxY = 4., minY = -4.;
-	int num_polygon = 50;
-	double stepX = (maxX - minX) / num_polygon, stepY = (maxY - minY) / num_polygon;
-
 	RECT my_rect_wnd;
 	GetClientRect(&my_rect_wnd);
 
@@ -132,19 +151,15 @@ void Win::OnPaint()
 	buf_gr.SetTransform(&matr);
 	buf_gr.Clear({ 255, 0, 0, 0 });
 
-	Pen my_pen({ 255, 255, 0, 0 }, 0.01), p(Color::Black, 0.1);
-	SolidBrush* my_brush;
+	Pen my_pen({ 255, 255, 0, 0 }, 0.01);
+	SolidBrush* my_brush = new SolidBrush(Color::Coral);;
 
-	std::vector<my_polygon> polygons;
-	for (double x = -3.; x < 3.; x += stepX)
-		for (double y = -3.; y < 3.; y += stepY) 
-			polygons.push_back(my_polygon(x, y, stepX, stepY, aroundY, aroundZ, maxX));
+	cur_polygons = RotateAllPolygons(polygons, aroundY, aroundZ, maxX);
 
-	SorterPolygons(polygons);
+	SorterPolygons(cur_polygons);
 
-	for (auto pl : polygons)
+	for (auto pl : cur_polygons)
 	{
-		my_brush = new SolidBrush(pl.color_polygon);
 		buf_gr.DrawPolygon(&my_pen, pl.GetPointF(), 4);
 		buf_gr.FillPolygon(my_brush, pl.GetPointF(), 4);
 	}
@@ -158,7 +173,7 @@ void Win::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
 
 	CFrameWnd::OnKeyDown(nChar, nRepCnt, nFlags);
-	double change_ugol = 2;
+	double change_ugol = 1;
 	if (nChar == VK_LEFT) aroundZ -= change_ugol;
 	if (nChar == VK_RIGHT) aroundZ += change_ugol;
 	if (nChar == VK_UP) aroundY -= change_ugol;
